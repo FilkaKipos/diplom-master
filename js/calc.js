ecb4b1928b24028c877a51bd420421c7f308fc99
ghostbang <rezidreta@gmail.com>
2024-06-08 23:31:03 +0500

    3213213213

--- js/calc.js
+++ js/calc.js
@@ -3,12 +3,13 @@ function calculateExercises() {
     const weightInput = document.getElementById('weight');
     const heightInput = document.getElementById('height');
     const goalInput = document.getElementById('goal');
+    const typeInput = document.getElementById('type');
 
     if (!weightInput.value) {
         alert('Введите свой вес!');
         return;
     }
-    
+
     if (!heightInput.value) {
         alert('Введите свой рост!');
         return;
@@ -17,6 +18,7 @@ function calculateExercises() {
     const weight = parseFloat(weightInput.value);
     const height = parseFloat(heightInput.value);
     const goal = goalInput.value;
+    const type = typeInput.value;
 
     if (isNaN(weight) || isNaN(height)) {
         alert('Вес и рост должны быть числовыми значениями!');
@@ -39,19 +41,19 @@ function calculateExercises() {
     switch (goal) {
         case "пресс":
             exercises = ["Скручивания", "Планка", "Велосипед"];
-            altExercises = ["Подъем ног лежа", "Косые скручивания"];
+            altExercises = ["Русский твист", "Подъем ног в висе"];
             break;
         case "руки":
             exercises = ["Отжимания", "Подтягивания", "Тяга гантелей"];
-            altExercises = ["Отжимания узким хватом", "Французский жим"];
+            altExercises = ["Тяга блока к груди", "Молотковый сгибание"];
             break;
         case "грудь":
             exercises = ["Жим лежа", "Отжимания на брусьях", "Разведение гантелей"];
-            altExercises = ["Жим гантелей на наклонной скамье", "Пуловер"];
+            altExercises = ["Отжимания с узким хватом", "Кроссовер"];
             break;
         case "ноги":
             exercises = ["Приседания", "Выпады", "Становая тяга"];
-            altExercises = ["Подъемы на носки", "Жим ногами"];
+            altExercises = ["Жим ногами", "Подъем на носки"];
             break;
         default:
             exercises = ["Отжимания", "Приседания", "Подтягивания"];
@@ -63,6 +65,8 @@ function calculateExercises() {
 
     let approaches = Math.round(weight / 10); // Исходное количество подходов
     let repetitions = Math.round((height / 100) * 2); // Исходное количество повторений
+
+    let workWeight = Math.round(weight * 0.3); // Исходный рабочий вес (30% от веса тела)
 
     if (gender === 'ж') {
         approaches = Math.round(approaches * 0.8); // Уменьшение нагрузки на 20% для женщин
@@ -76,13 +80,18 @@ function calculateExercises() {
             if (i === 2 || i === 5) {
                 weeklyResult += `<span class="day">${days[i]}:</span> Выходной<br>`;
             } else {
-                weeklyResult += `<span class="day">${days[i]}:</span> ${approaches} подхода, ${repetitions} повторения каждое упражнение - ${exercises.join(", ")}<br>`;
+                if (type === 'сила') {
+                    weeklyResult += `<span class="day">${days[i]}:</span> ${approaches} подхода, ${Math.round(repetitions * 0.7)} повторения каждое упражнение, Рабочий вес: ${workWeight} кг - ${exercises.join(", ")}<br>`;
+                } else if (type === 'выносливость') {
+                    weeklyResult += `<span class="day">${days[i]}:</span> ${Math.round(approaches * 1.5)} подхода, ${Math.round(repetitions * 0.5)} повторения каждое упражнение, Рабочий вес: ${workWeight} кг - ${exercises.join(", ")}<br>`;
+                }
                 weeklyResult += `Альтернативные упражнения: ${altExercises.join(", ")}<br>`;
             }
         }
 
         approaches = Math.round(approaches * 1.3); // Увеличение нагрузки на 1.3 раза
         repetitions = Math.round(repetitions * 1.3); // Увеличение нагрузки на 1.3 раза
+        workWeight = Math.round(workWeight * 1.03); // Увеличение рабочего веса на 3%
 
         if (gender === 'ж') {
             approaches = Math.round(approaches * 0.8); // Уменьшение нагрузки на 20% для женщин
@@ -97,6 +106,7 @@ function calculateExercises() {
     localStorage.setItem('weight', weight);
     localStorage.setItem('height', height);
     localStorage.setItem('goal', goal);
+    localStorage.setItem('type', type);
     localStorage.setItem('result', result);
     localStorage.setItem('weeklyResult', weeklyResult);
 
